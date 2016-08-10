0e3efd8c386b253412ef3d2d06f80c8d193a65cc
dan2kx <danlaptop2kx@hotmail.com>
2016-08-10 17:56:00 +0100

    Update app.jsx

--- app/app.jsx
+++ app/app.jsx
@@ -1,9 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
-var Main = require('Main');
-var Timer = require('Timer');
-var Countdown = require('Countdown');
 
 //load foundation
 $(document).foundation();
